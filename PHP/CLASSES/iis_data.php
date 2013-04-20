90272da41a6799559e454dd02ec89d2643cf5d17
Marejean Granaderos Perpinosa <granaderos@icotpict-MG-63MI-7059.(none)>
2013-04-20 14:30:45 +0800

    login.php

---
+++ PHP/CLASSES/iis_data.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+
+    include_once "database_connection.php";
+
+    class Iis_data extends Database_connection {
+
+        function check_user($username_entered, $password_entered) {
+            $this->open_connection();
+
+            $select_statement = $this->db_holder->prepare("SELECT *
+                                                             FROM employees AS e,
+                                                                  accounts AS a
+                                                             WHERE e.employee_id = a.employee_id AND
+                                                                   a.username = ? AND
+                                                                   a.password = ?;");
+            $select_statement->execute(array($username_entered, $password_entered));
+
+            if($select_statement->fetch()) {
+                return true;
+            }
+
+            $this->close_connection();
+        }
+
+    }
